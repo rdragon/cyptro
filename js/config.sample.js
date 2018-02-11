a78f00307d5491e330296308f2efd28dfe6216f9
Rik Steenkamp <c5fstn@ewps.nl>
2018-02-11 09:24:31 +0100

    Use different salt for each password, not for each entry

--- js/config.sample.js
+++ js/config.sample.js
@@ -3,6 +3,7 @@ export default {
   blockSize: 128 / 8,
   hmacSize: 256 / 8,
   derivationCount: 10000,
+  saltSuffix: '96tNpQYEsaE427PRNlOc7N3WK',
   decipher: 'AES-CBC',
   hasher: 'sha256',
   entriesFile: 'entries.json',
