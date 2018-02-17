cd9a05be67f123923b57df53880fa1b213ddc65d
Rik Steenkamp <c5fstn@ewps.nl>
2018-02-17 07:05:34 +0100

    Hide url

--- js/config.sample.js
+++ js/config.sample.js
@@ -3,14 +3,14 @@ export default {
   blockSize: 128 / 8,
   hmacSize: 256 / 8,
   derivationCount: 10000,
-  saltSuffix: '96tNpQYEsaE427PRNlOc7N3WK',
+  saltSuffix: 'bGO2CmHKUUJobjJvsB6TATmHl',
   decipher: 'AES-CBC',
   hasher: 'sha256',
   entriesFile: 'entries.json',
   entrySuffix: '.txt',
   loginButtonText: 'Log in',
   decodeButtonText: 'Decode',
-  staticDataUrl: 'data',
-  apiUrl: 'api/api.php',
+  staticDataUrl: '/cyptro/data',
+  apiUrl: '/cyptro/api/api.php',
   logLevel: 'error'
 };
